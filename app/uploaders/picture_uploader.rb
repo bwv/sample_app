e1f9eeee8f48742ae2d5b9b4abff9589f7c4da7f
Niels <nielsjhdr@gmail.com>
2014-11-01 06:26:12 +0100

    Changed production upload storage to file b/c no Amazon/fog

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,9 +7,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  Rails.env.production? ? storage(:fog) : storage(:file) 
-   
-
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
